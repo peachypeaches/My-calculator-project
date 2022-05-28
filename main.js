75e562c23a156e39feb402d8d927353ec5831358
durobolu <durobolu@gmail.com>
2022-05-28 21:45:16 +0100

    my first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+function dis(val) {
+    document.getElementById("calc").value += val 
+}
+function solve() {
+    let x = document.getElementById("calc").value
+    let y = eval(x)
+    document.getElementById("calc").value = y
+}
+function clr() {
+    document.getElementById("calc").value = ""
+}
